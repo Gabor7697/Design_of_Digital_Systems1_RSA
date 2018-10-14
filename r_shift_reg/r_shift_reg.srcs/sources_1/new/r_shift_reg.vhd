08914526a900cbb3dfc17b4bf585f775476cddd5
Unknown <gaborpenzinger94@gmail.com>
2018-10-14 13:07:44 +0200

    Update

--- r_shift_reg/r_shift_reg.srcs/sources_1/new/r_shift_reg.vhd
+++ r_shift_reg/r_shift_reg.srcs/sources_1/new/r_shift_reg.vhd
@@ -5,6 +5,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity r_shift_reg is
     generic( n : natural := 256);
     Port ( data : in STD_LOGIC_VECTOR (n+1 downto 0);
+           rst : in std_logic;
+           clk : in std_logic;
            d_out : out STD_LOGIC_VECTOR (n downto 0));
 end r_shift_reg;
 
@@ -12,6 +14,13 @@ architecture Behavioral of r_shift_reg is
 
 begin
 
-d_out <= data(n+1 downto 1);
+process(rst,clk)
+begin   
+    if(rst = '1') then
+        d_out <= (others => '0');
+    elsif (clk'event and clk = '1') then    
+        d_out <= data(n+1 downto 1);
+    end if;
+end process;
 
 end Behavioral;
