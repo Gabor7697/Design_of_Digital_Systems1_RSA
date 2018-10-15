f8b26d9ea827c110914d28d8fee97db84514834f
Unknown <gaborpenzinger94@gmail.com>
2018-10-15 21:40:08 +0200

    Add wr_e signal and CLA_258

--- r_shift_reg/r_shift_reg.srcs/sources_1/new/r_shift_reg.vhd
+++ r_shift_reg/r_shift_reg.srcs/sources_1/new/r_shift_reg.vhd
@@ -7,6 +7,7 @@ entity r_shift_reg is
     Port ( data : in STD_LOGIC_VECTOR (n+1 downto 0);
            rst : in std_logic;
            clk : in std_logic;
+           wr_e : in std_logic;
            d_out : out STD_LOGIC_VECTOR (n downto 0);
            lsb_out : out std_logic);
 end r_shift_reg;
@@ -15,11 +16,11 @@ architecture Behavioral of r_shift_reg is
 
 begin
 
-process(rst,clk)
+process(rst,clk,wr_e)
 begin   
     if(rst = '1') then
         d_out <= (others => '0');
-    elsif (clk'event and clk = '1') then    
+    elsif ((clk'event and clk = '1') and wr_e = '1') then    
         d_out <= data(n+1 downto 1);
         lsb_out <= data(1);
     end if;
